25cf8583e195b0b26d00d256c31dd2a1e4f344b3
Karen Acosta <kacosta@jmbbp.com>
2016-12-19 15:27:50 -0400

    adding changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,17 @@
-$(document).ready(function(){
-   //alert("Ich bin jq nice to meet you yo!");
-   $("h6.sender-reply-option").click(function(){
-     $("div.sender-reply-body").hide("slow", function(){
-     });
-   });
+$(function(){
 
-   $('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
-});
+
+  var $h6 = $("h6.sender-reply-option");
+  var $divReply = $(".sender-reply-body");
+   $h6.on('click', function(){
+     console.log("you!");
+     if($divReply.hasClass('slide-up')){
+       $divReply.addClass('slide-down');
+       $divReply.removeClass('slide-up');
+     }else{
+       $divReply.removeClass('slide-down');
+       $divReply.addClass('slide-up', 1000, 'easeOutBounce');
+     }
+   });//end of click event on reply
 
 });//end of wrap up
