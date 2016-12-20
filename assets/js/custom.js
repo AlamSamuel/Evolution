4934f7011c6cb1966a9e5fd1d9dd1cb2e8b17c3f
Karen Acosta <kacosta@jmbbp.com>
2016-12-20 14:15:39 -0400

    commiting a commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,15 +3,27 @@ $(function(){
 
   var $h6 = $("h6.sender-reply-option");
   var $divReply = $(".sender-reply-body");
-   $h6.on('click', function(){
-     console.log("you!");
-     if($divReply.hasClass('slide-up')){
-       $divReply.addClass('slide-down');
-       $divReply.removeClass('slide-up');
-     }else{
-       $divReply.removeClass('slide-down');
-       $divReply.addClass('slide-up', 1000, 'easeOutBounce');
-     }
-   });//end of click event on reply
+
+   /*$h6.on('click', function(){
+     $divReply.height('0px');
+   });//end of click event on reply*/
+
+  //  $h6.on('click', function(){
+  //    $divReply.toggle(function(){
+  //      $(this).animate({height: 168}, 500)
+  //    },function(){
+  //      $(this).animate({height: 0}, 500)
+  //    });
+  //  });
+
+  $h6.click((function() {
+    var i = 0;
+    return function() {
+        $(this).animate({
+            height: (++i % 2) ? 40 : 10
+        }, 200);
+    }
+})());
+
 
 });//end of wrap up
